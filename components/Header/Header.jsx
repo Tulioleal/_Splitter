32bdadf6b35895b87c2bbda581b1c269bbde2894
Tulio <tulioleal13@gmail.com>
2021-02-04 10:56:20 -0300

    Header component (style and structure)

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,7 +4,11 @@ import styles from './Header.module.scss'
 const Header = () => {
     return (
         <div className={styles.container}>
-            
+            <img
+                src="/Splitter.png"
+                alt="Splitter logo"
+                className={styles.logo}
+            />
             <Link href='/'>
                 <h1>SPLITTER</h1>
             </Link>

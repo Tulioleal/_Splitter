32743f8d51be175594cd13c5f110f33ad169e560
Tulio <tulioleal13@gmail.com>
2021-02-04 10:56:08 -0300

    Footer component (style and structure)

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            <a href="https://tulioleal.netlify.app/">
+            Tulio Leal. 
+            </a> &nbsp; All Rights Reserved. 2021.
+        </div>
+    )
+}
+
+export default Footer

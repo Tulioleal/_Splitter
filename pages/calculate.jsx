56834e846fc3a7c3d0fdcf15478184da2e0ac182
Tulio <tulioleal13@gmail.com>
2021-02-04 20:21:14 -0300

    structures and styles to calculate

--- pages/calculate.jsx
+++ pages/calculate.jsx
@@ -1,7 +1,22 @@
+import styles from '../styles/Calculate.module.scss'
+
 const calculate = () => {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <h2>Gathering name</h2>
+
+            <div className={styles.results}>
+                <h3>Total</h3>
+                <span>asxz</span>
+            </div>
+
+            <div className={styles.results}>
+                <h3>Each<br/>Friend</h3>
+                <span>sac</span>
+            </div>
+
+            <h3 className={styles.title}>Results</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta alias quidem nihil ex, aspernatur odit?</p>
         </div>
     )
 }

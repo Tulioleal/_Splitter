7676054894867f5b73a8e69089ebeeb6c0df26e9
Tulio <tulioleal13@gmail.com>
2021-02-04 10:58:53 -0300

    Add Home page structure and styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,25 @@
 import Layout from '../components/Layout/Layout'
+import Button from '../components/Button/Button'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <div>
       <Layout>
-
+        <div className={styles.container}>
+          <h2>Welcome</h2>
+          <p>
+            The magic of Splitter is that we <span>think</span> so you don’t have to.
+          </p>
+          <Button
+            href='/about'
+            title='About te App'
+          />
+          <Button 
+            href='/main'
+            title='New Gathering'
+          />
+        </div>
       </Layout>
     </div>
   )

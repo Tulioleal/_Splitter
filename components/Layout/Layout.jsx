fb771cd6862b9ba0184550aaa50c4d71fee46113
Tulio <tulioleal13@gmail.com>
2021-02-04 10:56:33 -0300

    Layout component (style and structure)

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,19 +1,18 @@
 import styles from "./Layout.module.scss";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div className={styles.container}>
-        <img
-            src="/Splitter.png"
-            alt="Splitter logo"
-            className={styles.logo}
-        />
+    <div className={styles.container}>
+      <div className={styles.header}>
         <Header />
       </div>
-      {children}
-    </>
+      <main className={styles.main}>
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 };
 

d391b20893c1461cd3dd969168dee19acfc19d07
Tulio <tulioleal13@gmail.com>
2021-02-05 20:47:43 -0300

    add save logic, state managment & query to result

--- pages/newGathering.jsx
+++ pages/newGathering.jsx
@@ -2,8 +2,9 @@ import Button from "../components/Button/Button";
 import ButtonLink from "../components/ButtonLink/ButtonLink";
 import styles from "../styles/NewGathering.module.scss";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
-const Friend = ({ placeholder }) => {
+/* const Friend = ({ placeholder, indexN }) => {
     const [name, setName] = useState("")
     const [amount, setAmount] = useState(0)
 
@@ -13,6 +14,7 @@ const Friend = ({ placeholder }) => {
                 type="text"
                 placeholder={placeholder}
                 value={name}
+                name="name"
                 onChange={e => setName(e.target.value)}
                 className={styles.input}
             />
@@ -20,52 +22,103 @@ const Friend = ({ placeholder }) => {
                 type="number"
                 placeholder="Money Spent"
                 value={amount}
+                name="amount"
                 onChange={e => setAmount(e.target.value)}
                 className={styles.input}
             />
         </div>
     )
-}
-
-const FriendAE = {
-  name: "",
-  amount: "",
-};
+} */
 
 const newGathering = () => {
+  const router = useRouter();
 
-  const [friends, setFriends] = useState([FriendAE, FriendAE]);
+  const [gathName, setGathName] = useState("");
+
+  const [friends, setFriends] = useState([
+    {
+      name: "",
+      amount: "",
+    },
+    {
+      name: "",
+      amount: "",
+    },
+  ]);
 
   const addFriend = () => {
-    if(friends.length >= 15) return alert('Sory 15 our limit :(')
-    const newFriends = [...friends, FriendAE]
-    setFriends(newFriends)
-  };
-
-  const removeFriend = () => {
-    if( friends.length <= 2 ) return alert('You need a least one friend')
-    const newFriends = friends.splice(0 , friends.length -1)
+    if (friends.length >= 15) {
+      return alert("Sory 15 our limit :(");
+    }
+    const newFriends = [...friends, { name: "", amount: 0 }];
     setFriends(newFriends);
   };
 
-  const sendGathering= e =>{
-      e.preventDefault()
+  const removeFriend = () => {
+    if (friends.length <= 2) {
+      return alert("You need a least one friend");
+    }
+    const newFriends = friends.splice(0, friends.length - 1);
+    setFriends(newFriends);
+  };
 
-      console.log('algo')
-  }
+  const sendGathering = () => {
+    if ( gathName && friends && localStorage.getItem(`splitter-${gathName}`) === null ) {
+      localStorage.setItem(`splitter-${gathName}`, JSON.stringify(friends));
+      return true;
+    } else {
+      alert("no");
+      return false;
+    }
+  };
+
+  const handleInputChange = (index, event) => {
+    const values = [...friends];
+    if (event.target.name === "name") {
+      values[index].name = event.target.value;
+    } else {
+      values[index].amount = event.target.value;
+    }
+
+    setFriends(values);
+  };
 
   return (
-    <form onSubmit={sendGathering} className={styles.container}>
+    <div className={styles.container}>
       <input
         type="text"
         placeholder="Gathering name"
         className={styles.input}
+        onChange={(e) => setGathName(e.target.value)}
       />
 
-      <Button title="Add a friend" />
+      <Button onClick={addFriend} title="Add a friend" />
 
-      {friends.map((index) => (
-        <Friend key={index} placeholder={ index == 0 ? 'Your name' : "Friend's name"} />
+      {friends.map((friend, index) => (
+        <div className={styles.friend}>
+          <input
+            type="text"
+            placeholder={index === 0 ? "First name" : "Friend's name"}
+            value={friend.name}
+            name="name"
+            id="name"
+            onChange={(event) => handleInputChange(index, event)}
+            className={styles.input}
+            key={`${index}-name`}
+            required={true}
+          />
+          <input
+            type="number"
+            placeholder="Money Spent"
+            value={friend.amount}
+            name="amount"
+            id="amount"
+            onChange={(event) => handleInputChange(index, event)}
+            className={styles.input}
+            key={`${index}-amount`}
+            required={true}
+          />
+        </div>
       ))}
 
       <div className={styles.row}>
@@ -73,9 +126,14 @@ const newGathering = () => {
         <Button onClick={removeFriend} title="-" />
       </div>
 
-      {/* <ButtonLink title="Calculate" href="/calculate" /> */}
-      <button type='submit'></button>
-    </form>
+      <Button
+        onClick={(e) => {
+          e.preventDefault
+          sendGathering() ? router.push(`/results/${gathName}`) : router.push(`/newGathering`)
+        }}
+        title="Calculate"
+      />
+    </div>
   );
 };
 

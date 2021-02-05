db4f50a26811a1412eade3ccbacbc557fe173f04
Tulio <tulioleal13@gmail.com>
2021-02-05 20:48:18 -0300

    add type props

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,13 +1,19 @@
 import styles from './Button.module.scss'
 
-const Button = ({ title, onClick }) => {
+const Button = ({ title, onClick, type }) => {
     return (
         <button
             onClick={onClick}
-            className={styles.btn}>
+            className={styles.btn}
+            type={type}
+            >
             {title}
         </button>
     )
+}
+
+Button.defaultProps = {
+    type: 'button'
 }
 
 export default Button
--- components/ButtonLink/ButtonLink.jsx
+++ components/ButtonLink/ButtonLink.jsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import styles from './ButtonLink.module.scss'
 
-const ButtonLink = ({ title, href }) => {
+const ButtonLink = ({ title, href, type }) => {
     return (
         <Link
             href={href}
         >
-            <button className={styles.btn}>
+            <button type={type} className={styles.btn}>
                 {title}
             </button>
         </Link>

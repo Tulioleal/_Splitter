70a318d143e8d423079acd23b8414b258aa08ce2
Tulio <tulioleal13@gmail.com>
2021-02-15 18:03:53 -0300

    add root empty aray if no DB

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import Layout from '../components/Layout/Layout'
 import '../styles/globals.scss'
+import { useEffect } from 'react'
+import localforage from 'localforage'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+
+    let init 
+    
+    (async () => {
+    
+      await localforage.getItem( 'gatherings' )
+      .then( data => init = data )
+      .catch( err => init = err )
+
+      init === null && setDB()
+    })()
+
+    const setDB = () => {
+      localforage.setItem('gatherings', [])
+      .then( (data) => console.log(data) )
+      .catch( err => console.log(err) )
+    }
+
+  }, [])
+
   return (
     <Layout>
       <Component {...pageProps} />

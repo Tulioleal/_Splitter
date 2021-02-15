4e3b4e2022ec22499ac808ce796312964d724b06
Tulio <tulioleal13@gmail.com>
2021-02-15 18:05:42 -0300

    fix localforae bugs

--- pages/results/calculate.jsx
+++ pages/results/calculate.jsx
@@ -3,34 +3,37 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Button from '../../components/Button/Button'
 import ButtonLink from '../../components/ButtonLink/ButtonLink'
-import localforage from "localforage";
 
 const calculate = () => {
   const router = useRouter();
 
   const [count, setCount] = useState(0);
 
-  const [task, settask] = useState({});
-
   const [friends, setFriends] = useState([]);
 
   const [results, setResults] = useState([]);
 
-  const total = (gath) => {
-    let total = 0;
-    gath.friends.forEach(
-      (element) => (total = total + parseFloat(element.amount))
+  const [total, setTotal] = useState(0)
+  
+  const [each, setEach] = useState(0)
+  
+  let totalVar = 0
+  let eachVar = 0
+
+  const totalFnc = (friends) => {
+    friends.forEach(
+      friend => (totalVar = totalVar + parseFloat(friend.amount))
     );
-    gath.total = total;
+    return setTotal(totalVar)
   };
 
-  const eachFriend = (gath) => {
-    let each = gath.total / parseFloat(gath.friends.length);
-    gath.each = each.toFixed(2);
+  const eachFriend = (friends) => {
+    eachVar = totalVar / friends.length;
+    setEach(parseFloat(eachVar.toFixed(2)))
   };
 
-  const sortAndSet = (gath) => {
-    let friends = gath.friends.sort((a, b) => {
+  const sortAndSet = (friends) => {
+    let friends1 = friends.sort((a, b) => {
       if (parseInt(a.amount) > parseInt(b.amount)) {
         return -1;
       }
@@ -40,22 +43,19 @@ const calculate = () => {
       return 0;
     });
 
-    friends.map((friend) => {
-      friend.amount = parseInt(friend.amount) - parseFloat(gath.each);
+    friends1.map((friend) => {
+      friend.amount = parseInt(friend.amount) - eachVar;
     });
 
-    setFriends(friends);
+    setFriends(friends1);
   };
 
   const compareResults = () => {
 
-    const negFnc = (friends.filter( friend => friend.amount < 0)).reverse()
-    const posFnc = friends.filter( friend => friend.amount > 0);
+    const neg1 = (friends.filter( friend => friend.amount < 0)).reverse()
+    const pos1 = friends.filter( friend => friend.amount > 0);
     
     const arr = [];
-
-    let neg1 = [].concat(negFnc)
-    let pos1= [].concat(posFnc)
 
     for (let i = 0; i < pos1.length; i++) {
       let num = 0;
@@ -70,19 +70,16 @@ const calculate = () => {
             pos1[i].amount = res;
             neg1[num].amount = 0;
             num++;
-            console.log(res, 1)
           } else if (res < 0) {
             pos1[i].amount != 0 &&
               arr.push(`${neg1[num].name} pays ${pos1[i].amount.toFixed(2)} to ${pos1[i].name}`);
             pos1[i].amount = 0;
             neg1[num].amount = res;
-            console.log(res, 2)
           } else {
             pos1[i].amount != 0 &&
               arr.push(`${neg1[num].name} pays ${pos1[i].amount.toFixed(2)} to ${pos1[i].name}`);
             pos1[i].amount = 0;
             neg1[num].amount = 0;
-            console.log(res, 3)
           }
         } else {
           return;
@@ -90,28 +87,27 @@ const calculate = () => {
       }
     }
     setResults(arr);
-    console.log(arr, count)
   };
 
   useEffect(() => {
     try {
-      localforage.getItem(router.query.gathName).then((returnedGath) => {
-        if (returnedGath === null) {
-          return router.push({ pathname: `/newGathering` });
-        } else {
-          total(returnedGath);
-          eachFriend(returnedGath);
-          settask(returnedGath);
-          sortAndSet(returnedGath);
-          compareResults()
-        }
-      });
+      let queryFriends = JSON.parse(router.query.friends)
+
+      if (queryFriends === null) {
+        return router.push({ pathname: `/newGathering` });
+      } else {
+        totalFnc(queryFriends);
+        eachFriend(queryFriends);
+        sortAndSet(queryFriends);
+        compareResults()
+      }
+   
     } catch (err) {
       console.log(err);
     }
   }, []);
 
-  const cls = () => {
+  const compare = () => {
     if (count == 0) {
       compareResults()
       setCount(1)
@@ -126,7 +122,7 @@ const calculate = () => {
 
       <div className={styles.results}>
         <h3>Total</h3>
-        <span>{task.total}</span>
+        <span>{total}</span>
       </div>
 
       <div className={styles.results}>
@@ -135,7 +131,7 @@ const calculate = () => {
           <br />
           Friend
         </h3>
-        <span>{task.each}</span>
+        <span>{each}</span>
       </div>
 
       <ul className={styles.list}>
@@ -156,7 +152,7 @@ const calculate = () => {
       </ul>
 
       <Button
-        onClick={cls}
+        onClick={compare}
         title='Show Suggestions'
       />
       
@@ -168,15 +164,19 @@ const calculate = () => {
         }
       </ul>
 
-      <div className={styles.row}>
-        <ButtonLink
-          title='New Gathering'
-          href='/newGathering'
-        />
-        <ButtonLink
-          title='Gatherings'
-          href='/gatherings'
-        />
+      <div className={styles.buttons}>
+        <div className={styles.button}>
+          <ButtonLink
+            title='New Gath'
+            href='/newGathering'
+          />
+        </div>
+        <div className={styles.button}>
+          <ButtonLink
+            title='Your Gaths'
+            href='/gatherings'
+          />
+        </div>
       </div>
 
     </div>
